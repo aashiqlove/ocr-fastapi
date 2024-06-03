42a37097eac6f68cdb5917a1c4d1bc2d9c47d763
Aashik Ali <84761597+aashiqlove@users.noreply.github.com>
2024-06-03 15:17:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI, File, UploadFile
-     import easyocr
-     import uvicorn
-     import numpy as np
-     from PIL import Image
-     import io
+import easyocr
+import uvicorn
+import numpy as np
+from PIL import Image
+import io
 
-     app = FastAPI()
-     reader = easyocr.Reader(['en'])
+app = FastAPI()
+reader = easyocr.Reader(['en'])
 
-     @app.post("/")
-     async def read_text(file: UploadFile = File(...)):
-         contents = await file.read()
-         image = Image.open(io.BytesIO(contents))
-         image_np = np.array(image)
-         result = reader.readtext(image_np)
-         texts = [entry[1] for entry in result]
-         return {"texts": texts}
+@app.post("/")
+async def read_text(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    image_np = np.array(image)
+    result = reader.readtext(image_np)
+    texts = [entry[1] for entry in result]
+    return {"texts": texts}
 
-     if __name__ == "__main__":
-         uvicorn.run(app, host="0.0.0.0", port=3000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
